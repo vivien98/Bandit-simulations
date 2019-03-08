af591724dee0913a9965252b515c9b987e5a5d08
vivien98 <nadkarniv4198@gmail.com>
2019-03-08 17:00:31 +0530

    thompson sampling and ucb arm choosing functions done

--- sim.py
+++ sim.py
@@ -12,15 +12,16 @@ actualBernMeans = np.random.uniform(0,1,(N_users,N_arms))
 
 class user :
 	def __init__(self,nbu):
-		self.nbu = nbu
-		self.alpha = np.ones(N_arms)
+		self.nbu = nbu 						#neighbouring users in graph
+
+		self.alpha = np.ones(N_arms)    #beta params
 		self.beta = np.ones(N_arms)
 
-		self.est_mean = 0
-		self.ucb = 0
+		self.est_mean = np.zeros(N_arms)      #ucb algorithm params
+		self.ucb = np.zeros(N_arms)
 		self.num_pulled = np.zeros(N_arms)
 
-		self.regret = 0
+		self.regret = 0     # regret
 
 	def updateThompDistr(self,arm,bern):
 		self.alpha[arm] = self.alpha[arm] + bern
@@ -28,38 +29,57 @@ class user :
 
 	def updateUCBEst(self,arm,bern):
 		num_pulled[arm] += 1
-		self.est_mean = (self.est_mean*(num_pulled[arm] - 1) + bern)/(num_pulled[arm])
-		self.ucb = math.sqrt(ucb_alpha*math.log(t) / (2*num_pulled[arm]))
+		self.est_mean[arm] = (self.est_mean[arm]*(self.num_pulled[arm] - 1) + bern)/(self.num_pulled[arm])
+		self.ucb[arm] = math.sqrt(ucb_alpha*math.log(t) / (2*self.num_pulled[arm]))
 
-def updateThompGraph(users,arms,armPulled,userArrived):
+	def updateUCBTime(self):
+		for i in range(N_arms):
+			self.ucb[i] = math.sqrt(ucb_alpha*math.log(t) / (2*self.num_pulled[i]))
+
+
+
+def updateThompGraph(armPulled,userArrived):
 	reward = np.random.binomial(1,actualBernMeans[userArrived][armPulled])
 	inst_regret = np.amax(actualBernMeans[userArrived])-actualBernMeans[userArrived][armPulled]
 	userArrived.regret += inst_regret
 
 	userArrived.updateThompDistr(armPulled,reward)
 
-	for i in range(userArrived.nbu.length()):
+	for i in range(userArrived.nbu.size):
 		reward1 = np.random.binomial(1,actualBernMeans[userArrived.nbu[i]][armPulled])
 		users[userArrived.nbu[i]].updateThompDistr(armPulled,reward1)
 
-	for i in range(armGraph[armPulled].length()):
+	for i in range(armGraph[armPulled].size):
 		reward2 = np.random.binomial(1,actualBernMeans[userArrived][armGraph[armPulled][i]])
 		userArrived.updateThompDistr(armGraph[armPulled][i],reward2)
 
-def updateUCBGraph(users,arms,armPulled,userArrived):
+def updateUCBGraph(armPulled,userArrived):
 	reward = np.random.binomial(1,actualBernMeans[userArrived][armPulled])
 	inst_regret = np.amax(actualBernMeans[userArrived])-actualBernMeans[userArrived][armPulled]
 	userArrived.regret += inst_regret
 
 	userArrived.updateUCBEst(armPulled,reward)
 
-	for i in range(userArrived.nbu.length()):
+	for i in range(userArrived.nbu.size):
 		reward1 = np.random.binomial(1,actualBernMeans[userArrived.nbu[i]][armPulled])
 		users[userArrived.nbu[i]].updateUCBEst(armPulled,reward1)
 
-	for i in range(armGraph[armPulled].length()):
+	for i in range(armGraph[armPulled].size):
 		reward2 = np.random.binomial(1,actualBernMeans[userArrived][armGraph[armPulled][i]])
 		userArrived.updateUCBEst(armGraph[armPulled][i],reward2)
 
+def armToPullThomp(userArrived,armToPull):
+	generated = np.random.beta(userArrived.alpha,userArrived.beta,(N_arms))
+	armToPull = argmax(generated)
 
+def armToPullUCB(userArrived,armToPull):
+	armToPull = -1
+	userArrived.updateUCBTime()
+	for i in range(userArrived.num_pulled.size):
+		if num_pulled[i] == 0:
+			armToPull = i
+			break
 
+	if armToPull == -1:
+		generated = userArrived.est_mean + userArrived.ucb
+		armToPull = argmax(generated)
