f26247c2a21957154f9da9aa5d48f94ed84dbc88
vivien98 <nadkarniv4198@gmail.com>
2019-09-24 15:07:22 +0530

    optimization plotting  code for problem P1

--- BTP code/P1P2optimize.m
+++ BTP code/P1P2optimize.m
@@ -1,16 +1,22 @@
 %% Find policy s.t cumulative reward is maximized subject to constraint
 % This code finds the optimal value of (p,q) policy s.t reward at a deadline is maximized 
 % subject to the constraint that the proportion of users of preference 0 is greater than 
-% a certain threshold zThresh.
+% a certain threshold zThresh. 
+% Plots the obtained optimal policy vs values of zThresh.
 
 b00 = 0.8;
 b01 = 0.1;
 b10 = 0.5;
 b11 = 0.7;
 z0 = 0.5;
-zThresh = 0.65;
+zThresh = 0.6:0.01:0.85;
 aTotal = 20;
 T = 2000000;
+policyArr1 = [];
+policyArr2 = [];
+
+for i = zThresh
+
 pol = optimvar('pol',1,2,'LowerBound',0,'UpperBound',1);
 
 [obj,cons] = rewardAndProp(b00,b01,b10,b11,pol(1),pol(2),z0,T,aTotal);
@@ -19,13 +25,18 @@ obj = -obj;
 
 prob = optimproblem('Objective',obj);
 
-nlcons = cons >= zThresh;
+nlcons = cons >= i;
 
 prob.Constraints.circlecons = nlcons;
 
-showproblem(prob);
+%showproblem(prob);
 
 x0.pol = [0.5 0.5];
-[sol,fval,exitflag,output] = solve(prob,x0)
+[sol,fval,exitflag,output] = solve(prob,x0);
 
-sol.pol
+policyArr1 = [policyArr1,sol.pol(1)];
+policyArr2 = [policyArr2,sol.pol(2)];
+end
+figure(1)
+plot(zThresh,policyArr1,zThresh,policyArr2)
+
